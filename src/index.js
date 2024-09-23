a5e512074fcc6cdb56f21e743c38121bdcc103ef
bettercallmahin <bettercallmahin@gmail.com>
2024-09-23 18:48:19 +0600

    inline css

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const firstName = 'John';
-const lastName = 'Doe';
+const firstName = 'Mahin';
+const lastName = 'Muntasir';
+const date = new Date()
+const dateName = date.getDate();
+const monthName = date.getMonth();
+const yearName = date.getFullYear();
+
+//css inline styling
+
+const h1Style = {
+
+  fontFamily: "poppins",
+  fontSize: "36px",
+  backgroundColor: "cyan",
+  padding: "10px",
+  borderRadius: "10px",
+  textAlign: "center"
+
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <h1>Mahin Muntasir</h1>
-
+    <h1 style={h1Style}>TODO App</h1>
     <h2>{firstName} {lastName}</h2>
+    <p>
+      This is a To-Do App
+    </p>
+    <p>Today is: {dateName}/{monthName + 1}/{yearName}</p>
 
   </div>
 
