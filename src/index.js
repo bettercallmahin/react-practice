43f1bbb51c137be5a09a9dbdcca5ea759c6ae538
bettercallmahin <bettercallmahin@gmail.com>
2024-09-25 19:27:44 +0600

    created App.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-import Card from './components/Card';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
-    <h1 className="h1style largeText">TODO App</h1>
-    <Card /> {/* Card call */}
-    <Card /> {/* Card call */}
-    <Card /> {/* Card call */}
-
-  </div>
-
+  <App />
 );
