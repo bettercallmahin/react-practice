90abc4f4ace03dd9cb7975e4f01641b1c71a4d2a
bettercallmahin <bettercallmahin@gmail.com>
2025-01-06 18:52:23 +0600

    day1

---
+++ TypeScript/react-app/src/Message.tsx
@@ -0,0 +1,24 @@
+//function based component
+
+
+//PascalCasing
+function Message() {
+    // JSX: JavaScript XML
+    // check babeljs.io/repl
+
+    const name = ""; //keep it empty to show hello world, otherwise give any name to show it.
+
+    if(name)
+    return (
+        <div>
+            <h1>Hello, {name}!</h1>
+        </div>
+    );
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+        </div>
+    );
+}
+
+export default Message;
